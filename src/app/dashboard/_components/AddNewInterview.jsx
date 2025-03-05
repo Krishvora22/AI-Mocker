f661484d874cf4f5bdd308728a5590aa4f2a2152
Krish vora <krishvora22@gmail.com>
2025-03-05 21:11:15 +0530

    Initial commit

---
+++ src/app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,49 @@
+"use client"
+import React, { useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { chatSession } from '@/utils/GeminiAIModal'
+import { LoaderCircle } from 'lucide-react'
+import { db } from '@/utils/db'
+import { MockInterview } from '@/utils/schema'
+import { v4 as uuidv4 } from 'uuid';
+import { useUser } from '@clerk/nextjs'
+import moment from 'moment'
+import { useRouter } from 'next/navigation'
+
+const AddNewInterview = () => {
+    return (
+        <div>
+            <div className='p-10 border rounded-lg bg-secondary
+        hover:scale-105 hover:shadow-md cursor-pointer
+         transition-all border-dashed'
+            >
+                <h2 className='text-lg text-center'>+ Add New</h2>
+            </div>
+            <Dialog>
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
+        </div>
+    )
+}
+
+export default AddNewInterview
---
+++ utils/db.js
@@ -0,0 +1,14 @@
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { config } from "dotenv";
+
+// config({ path: ".env" }); // or .env.local
+
+// const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+// export const db = drizzle({ client: sql });
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema'
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+export const db = drizzle(sql,{schema});
